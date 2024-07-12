0fd03e1e592ae6ea2a49c1bc0fbfcbc51323b159
lastocheck <lastocheck@gmail.com>
2024-07-12 12:25:45 +0300

    add wiring to models

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,23 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
+    private Egg6 egg;
+
+    public Duck5(Egg6 egg) {
+        this.egg = egg;
+    }
+
+    @Autowired
+    public void setEgg(Egg6 egg) {
+        this.egg = egg;
+    }
 
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + this.egg;
     }
 }
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,19 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+    @Autowired
+    private final Deth8 death;
+
+    public Needle7(Deth8 death) {
+        this.death = death;
+    }
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле " + this.death;
     }
 }
